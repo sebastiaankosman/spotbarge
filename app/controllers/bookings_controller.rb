623a341172edcb13dcfbcae8df41cd3adb77a0cf
Dongjin Lee <djlee1509@gmail.com>
2019-08-27 18:15:15 +0200

    Working on the Controllers and Views. Need to work on the Routes first.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,7 @@
 class BookingsController < ApplicationController
   def show
+    @barge = Barge.find(params[:id])
+    @cargo = Cargo.find(params[:id])
+    @booking = Booking.find(params[:id])
   end
 end
--- app/controllers/cargos_controller.rb
+++ app/controllers/cargos_controller.rb
@@ -1,10 +1,26 @@
 class CargosController < ApplicationController
   def index
+    @cargos = Cargo.all
   end
 
   def new
+    @cargo = Cargo.new
   end
 
   def create
+    @cargo = Cargo.find(cargo_params)
+    if @cargo.save!
+      redirect_to cargos_index_path(@cargo)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cargo_params
+    params.require(:cargo)
+          .permit(:cargo_name, :volume, :load_location, :discharge_location,
+                  :cargo_load_window, :gas_free, :cargo_type)
   end
 end
