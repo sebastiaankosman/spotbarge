43bc348f9c15b423a182b6d13ecfe8029370a0fa
Dongjin Lee <djlee1509@gmail.com>
2019-08-28 15:01:04 +0200

    FEATURE working on the filters to filter out by different conditions.

--- app/controllers/barges_controller.rb
+++ app/controllers/barges_controller.rb
@@ -1,6 +1,27 @@
 class BargesController < ApplicationController
   def index
     @barges = Barge.all
+
+    if params[:barge_area]
+      barges_by_area = []
+      params[:barge_area].each do |area|
+        barges_by_area += @barges.by_area(area)
+      end
+      @barges = barges_by_area
+    end
+
+    if params[:barge_type]
+      barges_by_type = []
+      params[:barge_type].each do |type|
+        barges_by_type += @barges.by_product(type)
+      end
+      @barges = barges_by_type
+    end
+
+    @barges = @barges.by_volume(params[:capacity]) if params[:capacity]
+    @barges = @barges.by_load_window(params[:barge_load_window]) if params[:barge_load_window]
+    @barges = @barges.by_barge_area(params[:barge_area]) if params[:barge_area]
+    @barges
   end
 
   def new
--- app/models/barge.rb
+++ app/models/barge.rb
@@ -2,4 +2,10 @@ class Barge < ApplicationRecord
   belongs_to :broker, optional: true
   has_many :bookings_as_booker, class_name: 'Booking', :foreign_key => "booker_barge_id"
   has_many :bookings_as_booked, class_name: 'Booking', :foreign_key => "booked_barge_id"
+
+  scope :by_area, ->(area) { where("barge_area = ?", area) }
+  scope :by_product, ->(product) { where("barge_type = ? ", product) }
+  scope :by_volume, ->(volume) { where("capacity = ?", volume) }
+  scope :by_load_window, ->(load_window) { where("barge_load_window = ?", load_window) }
+  scope :by_barge_area, ->(location) { where("barge_area = ?", location) }
 end
