f85daa1238a81019894549e8a843481c8ab56fff
Dongjin Lee <djlee1509@gmail.com>
2019-08-29 15:45:52 +0200

    FEATURE started creating the booking functionality.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,28 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @barge = Barge.find(params[:id])
+    if @booking.save!
+      redirect_to barge_booking_path(@barge, @booking)
+    else
+      render :new
+    end
+  end
+
   def show
     @barge = Barge.find(params[:id])
     @cargo = Cargo.find(params[:id])
     @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:freight_rate, :free_hours, :demurrage_rate, :short_notice, :comment)
   end
 end
